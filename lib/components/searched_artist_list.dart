746e86575102bb3ee9758010cadbcdde1ee821ff
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-05-26 00:52:45 +0200

    add recent search list

--- lib/components/searched_artist_list.dart
+++ lib/components/searched_artist_list.dart
@@ -1,79 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:hci_a2_app/provider/artist.dart';
 
 class SearchedArtistList extends StatelessWidget {
   final _artistStream = FirebaseFirestore.instance
       .collection("SearchedArtists")
-      .orderBy("name")
-      .snapshots()
-      .distinct();
+      .orderBy("time")
+      .snapshots();
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.white,
-        child: StreamBuilder(
-          stream: _artistStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text(":(");
-            }
+    return Container(
+      color: Colors.white,
+      child: StreamBuilder(
+        stream: _artistStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error occurred loading data.");
+          }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
 
-            final List<Widget> result = snapshot.data.docs.map<Widget>((doc) {
-              return ListTile(
-                title: Text(doc["name"]),
-              );
-            }).toList();
-
-            return ListView.separated(
-              itemCount: result.length,
-              itemBuilder: (_, i) => result[i],
-              separatorBuilder: (_, __) => Divider(),
+          final List<Widget> result = snapshot.data.docs.map<Widget>((doc) {
+            return ListTile(
+              title: Text(doc["name"]),
             );
-          },
-        ),
+          }).toList();
+
+          return ListView.separated(
+            itemCount: result.length,
+            itemBuilder: (_, i) => result[i],
+            separatorBuilder: (_, __) => Divider(),
+          );
+        },
       ),
     );
   }
 }
-
-// class UserInformation extends StatefulWidget {
-//   @override
-//   _UsetInfomation createState() => _UserInfomationState();
-// }
-
-// class _UserInformationState extends State<UserInformation> {
-//   final Stream<QuerySnapshot> _usersStream =
-//       FirebaseFirestore.instance.collection('users').snapshots();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<QuerySnapshot>(
-//       stream: _usersStream,
-//       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//         if (snapshot.hasError) {
-//           return Text('Something went wrong');
-//         }
-
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return Text("Loading");
-//         }
-
-//         return new ListView(
-//           children: snapshot.data.docs.map((DocumentSnapshot document) {
-//             return new ListTile(
-//               title: new Text(document.data()['full_name']),
-//               subtitle: new Text(document.data()['company']),
-//             );
-//           }).toList(),
-//         );
-//       },
-//     );
-//   }
-// }
--- lib/firestore/functions.dart
+++ lib/firestore/functions.dart
@@ -1,17 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hci_a2_app/provider/artist.dart';
 
-void saveSearchedArtist(Artist artist) {
-  final collectionRef =
+class FirebaseFunctions {
+  // used for all functions
+  static final searchedCollectionRef =
       FirebaseFirestore.instance.collection("SearchedArtists").withConverter(
             // dont have to use json not
             fromFirestore: (snapshot, _) => Artist.fromJsonAPI(snapshot.data()),
             toFirestore: (providedArtist, _) => providedArtist.toJsonFirebase(),
           );
 
-  collectionRef
-      // directly pass artist because of converter
-      .add(artist)
-      .then((value) => print("success"))
-      .catchError((error) => print("fail"));
+  // used for all functions
+  static final favCollectionRef =
+      FirebaseFirestore.instance.collection("FavouriteArtists").withConverter(
+            // dont have to use json not
+            fromFirestore: (snapshot, _) => Artist.fromJsonAPI(snapshot.data()),
+            toFirestore: (providedArtist, _) => providedArtist.toJsonFirebase(),
+          );
+
+  static void saveSearchedArtist(Artist artist) {
+    searchedCollectionRef
+        // sets doc id to artist id
+        // this way searched artists are distinct
+        .doc("${artist.id}")
+        // directly pass artist because of converter
+        .set(artist)
+        .then((value) => print("success"))
+        .catchError((error) => print("fail"));
+    searchedCollectionRef.doc("${artist.id}").update({"time": Timestamp.now()});
+  }
+
+  static void addFav(Artist artist) async {
+    await favCollectionRef.doc(artist.id).set(artist);
+  }
+
+  static void deleteFav(Artist artist) async {
+    await favCollectionRef.doc("${artist.id}").delete();
+  }
 }
